5daceea25cd60f5b250c117b90ecddadbe17a2f7
SynergySINE <limgha.e@gmail.com>
2017-10-23 10:29:28 +0100

    Added return values to functions to simplify error management

--- API/scanner.py
+++ API/scanner.py
@@ -16,6 +16,7 @@ def request_git_api(request_url):
         res.raise_for_status()
     except Exception as err:
         logger.error(err)
+        return False
 
     return res
 
@@ -24,7 +25,7 @@ def git_scan(user):
     request_url = '{}/users/{}/repos'.format(url, user)
     res_user_repos = request_git_api(request_url)
     if not res_user_repos:
-        return None
+        return False
     json_user_repos = res_user_repos.json()
 
     logger.info("fetching languages url")
@@ -52,6 +53,7 @@ def fetch_available_resources(language):
     })
     except pm.errors.OperationFailure as err:
         logger.error(err)
+        return False
 
     for item in current_available_resources:
         available_resources.append(item)
@@ -62,7 +64,10 @@ def fetch_available_resources(language):
 def create_reading_list(user_languages):
     resource_list = []
     for language in user_languages:
-        resource_list.append(fetch_available_resources(language))
+        result = fetch_available_resources(language)
+        if not result:
+            return False
+        resource_list.append(result)
 
     return resource_list
 
@@ -74,17 +79,18 @@ def store_reading_list(tmp_reading_list, username):
     })
     except pm.errors.OperationFailure as err:
         logger.error(err)
+        return False
 
     if response_user is not None:
         tmp_db_rl = response_user['new_reading_list']
     else:
-        tmp_db_rl = []
+        return False
 
     reading_list = [item for sublist in tmp_reading_list for item in sublist]
     update_query = {
         '$set':{
-            'last_reading_list':tmp_db_rl,
-            'new_reading_list':reading_list
+            'last_reading_list': tmp_db_rl,
+            'new_reading_list': reading_list
         }
     }
     try:
@@ -94,16 +100,20 @@ def store_reading_list(tmp_reading_list, username):
                        update_query, upsert=True)
     except pm.errors.OperationFailure as err:
         logger.error(err)
+        return False
+
+    return True
 
 
 def get_user_info():
     try:
         user_list = db.user.find({
-            'github_username':True,
-            'token':True
+            'github_username': True,
+            'token': True
         })
     except pm.errors.OperationFailure as err:
         logger.error(err)
+        return False
 
     if user_list.count() is not None:
         json_user_list = user_list.json()
@@ -113,10 +123,22 @@ def get_user_info():
 
 def manual_scan(user):
     git_results = git_scan(user)
+    if not git_results:
+        logger.error("git_scan() returned False")
+        return False
+
     logger.info("Finished scanning")
+
     reading_list = create_reading_list(git_results)
+    if not reading_list:
+        logger.error("create_reading_list() returned False")
+        return False
+
     logger.info("Finished creating new reading list")
-    store_reading_list(reading_list, user)
+    result = store_reading_list(reading_list, user)
+    if not result:
+        logger.error("store_reading_list() returned False")
+
     print("Done")
 
     return True
@@ -124,6 +146,9 @@ def manual_scan(user):
 
 def automatic_scan():
     user_list = get_user_info()
+    if not user_list:
+        logger.error("get_user_info() returned False")
+        return False
     for user in user_list:
         manual_scan(user['github_username'])
          
