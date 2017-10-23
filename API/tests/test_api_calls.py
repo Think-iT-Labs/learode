90c1a5468e3cf233284935394521301ab205c4c9
SynergySINE <limgha.e@gmail.com>
2017-10-23 10:45:53 +0100

    Revert "Fixed indent for test_api_calls.py"
    This reverts commit b1838d1a37d35bb17b0e3b9f117dc3683547850e.

--- API/tests/test_api_calls.py
+++ API/tests/test_api_calls.py
@@ -9,7 +9,7 @@ logger = create_logger()
 print("Running Endpoint Tester....\n")
 address = raw_input("Enter the server's address\n Default: 'http://localhost:5000':   ")
 if address == '':
-    address = 'http://localhost:5000'
+	address = 'http://localhost:5000'
 
 
 # Test scan
@@ -32,60 +32,61 @@ print("Making a GET request to /scan...")
 
 # test fetch user
 print("Making a GET request to /user...")
-url = address + "/user/SynergySINE"
-try:
-    res = requests.get(url, timeout=5)
-    res.raise_for_status()
-except Exception as err:
-    logger.error(err)
 
-if not res:
-    return None
-json_res = res.json()
-assert json_res['github_username'] == "SynergySINE"
+    url = address + "/user/SynergySINE"
+	try:
+        res = requests.get(url, timeout=5)
+        res.raise_for_status()
+    except Exception as err:
+        logger.error(err)
+
+    if not res:
+        return None
+    json_res = res.json()
+    assert json_res['github_username'] == "SynergySINE"
 
 # test fetch resource
 print("Making a GET request to /resource...")
 
-url = address + "/resource/1"
-try:
-    res = requests.get(url, timeout=5)
-    res.raise_for_status()
-except Exception as err:
-    logger.error(err)
+    url = address + "/resource/1"
+	try:
+        res = requests.get(url, timeout=5)
+        res.raise_for_status()
+    except Exception as err:
+        logger.error(err)
 
-if not res:
-    return None
-json_res = res.json()
-assert json_res['title'] == "Intermediate Python"
+    if not res:
+        return None
+    json_res = res.json()
+    assert json_res['title'] == "Intermediate Python"
 
 # test insert resource
 print("Making a POST request to /resource...")
 
-url = address + "/resource/1"
-try:
-    res = requests.get(url, timeout=5)
-    res.raise_for_status()
-except Exception as err:
-    logger.error(err)
+    url = address + "/resource/1"
+	try:
+        res = requests.get(url, timeout=5)
+        res.raise_for_status()
+    except Exception as err:
+        logger.error(err)
 
-if not res:
-    return None
-json_res = res.json()
-assert json_res['response'] == True
+    if not res:
+        return None
+    json_res = res.json()
+    assert json_res['response'] == True
 
 # test insert user
 print("Making a POST request to /user...")
 
-url = address + "/user/1"
-try:
-    res = requests.get(url, timeout=5)
-    res.raise_for_status()
-except Exception as err:
-    logger.error(err)
+    url = address + "/user/1"
+	try:
+        res = requests.get(url, timeout=5)
+        res.raise_for_status()
+    except Exception as err:
+        logger.error(err)
 
-if not res:
-    return None
-json_res = res.json()
-assert json_res['response'] == True
+    if not res:
+        return None
+    json_res = res.json()
+    assert json_res['response'] == True
 
