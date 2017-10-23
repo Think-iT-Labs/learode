c6c93bbd93f8b05fecdcc87ab587af7df2b89afa
SynergySINE <limgha.e@gmail.com>
2017-10-23 10:48:07 +0100

    Fixed invalid indentation and multiple 'Return None' outisde of function in API/tests/test_api_calls.py

--- API/tests/test_api_calls.py
+++ API/tests/test_api_calls.py
@@ -9,84 +9,86 @@ logger = create_logger()
 print("Running Endpoint Tester....\n")
 address = raw_input("Enter the server's address\n Default: 'http://localhost:5000':   ")
 if address == '':
-	address = 'http://localhost:5000'
+    address = 'http://localhost:5000'
 
 
 # Test scan
 
 print("Making a GET request to /scan...")
 
-    url = address + "/scan/SynergySINE"
-	try:
-        res = requests.get(url, timeout=5)
-        res.raise_for_status()
-    except Exception as err:
-        logger.error(err)
+url = address + "/scan/SynergySINE"
+try:
+    res = requests.get(url, timeout=5)
+    res.raise_for_status()
+except Exception as err:
+    logger.error(err)
 
-    if not res:
-        return None
+if not res:
+    raise ValueError("Variable Res == False")
 
-    json_res = res.json()
-    assert res['response'] == True
+json_res = res.json()
+assert res['response'] == True
 
 
 # test fetch user
 print("Making a GET request to /user...")
+url = address + "/user/SynergySINE"
+try:
+    res = requests.get(url, timeout=5)
+    res.raise_for_status()
+except Exception as err:
+    logger.error(err)
 
-    url = address + "/user/SynergySINE"
-	try:
-        res = requests.get(url, timeout=5)
-        res.raise_for_status()
-    except Exception as err:
-        logger.error(err)
+if not res:
+    raise ValueError("Variable Res == False")
 
-    if not res:
-        return None
-    json_res = res.json()
-    assert json_res['github_username'] == "SynergySINE"
+json_res = res.json()
+assert json_res['github_username'] == "SynergySINE"
 
 # test fetch resource
 print("Making a GET request to /resource...")
 
-    url = address + "/resource/1"
-	try:
-        res = requests.get(url, timeout=5)
-        res.raise_for_status()
-    except Exception as err:
-        logger.error(err)
+url = address + "/resource/1"
+try:
+    res = requests.get(url, timeout=5)
+    res.raise_for_status()
+except Exception as err:
+    logger.error(err)
 
-    if not res:
-        return None
-    json_res = res.json()
-    assert json_res['title'] == "Intermediate Python"
+if not res:
+    raise ValueError("Variable Res == False")
+json_res = res.json()
+assert json_res['title'] == "Intermediate Python"
 
 # test insert resource
 print("Making a POST request to /resource...")
 
-    url = address + "/resource/1"
-	try:
-        res = requests.get(url, timeout=5)
-        res.raise_for_status()
-    except Exception as err:
-        logger.error(err)
+url = address + "/resource/1"
+try:
+    res = requests.get(url, timeout=5)
+    res.raise_for_status()
+except Exception as err:
+    logger.error(err)
 
-    if not res:
-        return None
-    json_res = res.json()
-    assert json_res['response'] == True
+if not res:
+    raise ValueError("Variable Res == False")
+
+json_res = res.json()
+assert json_res['response'] == True
 
 # test insert user
 print("Making a POST request to /user...")
 
-    url = address + "/user/1"
-	try:
-        res = requests.get(url, timeout=5)
-        res.raise_for_status()
-    except Exception as err:
-        logger.error(err)
+url = address + "/user/1"
+try:
+    res = requests.get(url, timeout=5)
+    res.raise_for_status()
+except Exception as err:
+    logger.error(err)
 
-    if not res:
-        return None
-    json_res = res.json()
-    assert json_res['response'] == True
+if not res:
+    raise ValueError("Variable Res == False")
+
+json_res = res.json()
+assert json_res['response'] == True
 
