7eb96e848710848a18e1cfdc734893dded91844d
SynergySINE <limgha.e@gmail.com>
2017-10-24 21:36:51 +0100

    Added form.css for login page style. Added form.css reference in index.html. Added new login page code in Index.jsx

--- ReactUI/js/react-components/Index.jsx
+++ ReactUI/js/react-components/Index.jsx
@@ -7,8 +7,7 @@ class Index extends React.Component {
         super(props)
         this.state = {
             connected: false,
-            username: '',
-            client_id: 'a2dfecffbb39b5f749fb'
+            username: ''
         }
     }
 
@@ -18,17 +17,53 @@ class Index extends React.Component {
         return (
             <div>
             {this.state.connected ? <App></App> :
-                <html>
-                  <head>
-                  </head>
-                  <body>
-                    <p>
-                      We're going to now talk to the GitHub API. Ready?
-                      <br/>
-                      <a className="btn-auth btn-github large" href={'https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=' + this.state.client_id}>Click here to begin!</a>
-                    </p>
-                  </body>
-                </html>
+                <div class="container">
+	<div class="row">
+		<form class="form-signin mg-btm">
+    	<h3 class="heading-desc">
+		<button type="button" class="close pull-right" aria-hidden="true">×</button>
+		Login to Bootsnipp</h3>
+		<div class="social-box">
+			<div class="row mg-btm">
+             <div class="col-md-12">
+                <a href="#" class="btn btn-primary btn-block">
+                  <i class="icon-facebook"></i>    Login with Facebook
+                </a>
+			</div>
+			</div>
+			<div class="row">
+			<div class="col-md-12">
+                <a href="#" class="btn btn-info btn-block" >
+                  <i class="icon-twitter"></i>    Login with Twitter
+                </a>
+            </div>
+          </div>
+		</div>
+		<div class="main">	
+        
+		<input type="text" class="form-control" placeholder="Email" autofocus>
+        <input type="password" class="form-control" placeholder="Password">
+		 
+        Are you a business? <a href=""> Get started here</a>
+		<span class="clearfix"></span>	
+        </div>
+		<div class="login-footer">
+		<div class="row">
+                        <div class="col-xs-6 col-md-6">
+                            <div class="left-section">
+								<a href="">Forgot your password?</a>
+								<a href="">Sign up now</a>
+							</div>
+                        </div>
+                        <div class="col-xs-6 col-md-6 pull-right">
+                            <button type="submit" class="btn btn-large btn-danger pull-right">Login</button>
+                        </div>
+                    </div>
+		
+		</div>
+      </form>
+	</div>
+</div>
             }
             </div>
         );
