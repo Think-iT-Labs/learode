b4ad43b78a231c59660b4f62caa5c6285bc23043
SynergySINE <limgha.e@gmail.com>
2017-10-27 15:30:04 +0100

    Added more resources to prepare_database.py

--- API/prepare_database.py
+++ API/prepare_database.py
@@ -15,7 +15,7 @@ test_user_query = [
     {
         "user_id":  1,
         "github_username":  "SynergySINE",
-        "token":  "0b98eba02250b467432dcefa32473d95f8fa8a07",
+        "github_access_token":  "",
         "new_reading_list":  [
         ],
         "last_reading_list":  [
@@ -35,7 +35,7 @@ test_resource_query = [
     {
         "res_id": 2,
         "title": "Python End-to-End Data analysis",
-        "url": "https: //www.packtpub.com/big-data-and-business-intelligence/python-end-end-data-analysis",
+        "url": "https://www.packtpub.com/big-data-and-business-intelligence/python-end-end-data-analysis",
         "language": "python",
         "level": "medium",
 	"read_by": []
@@ -43,7 +43,7 @@ test_resource_query = [
     {
         "res_id": 3,
         "title": "Learning Javascript",
-        "url": "https: //www.amazon.co.uk/Learning-JavaScript-Shelley-Powers/dp/8184042159",
+        "url": "https://www.amazon.co.uk/Learning-JavaScript-Shelley-Powers/dp/8184042159",
         "language": "javascript",
         "level": "beginner";
 	"read_by": []
@@ -51,11 +51,117 @@ test_resource_query = [
     {
         "res_id": 4,
         "title": "Learn HTML at MDN",
-        "url": "https: //developer.mozilla.org/en-US/docs/Web/HTML/Element/article",
+        "url": "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/article",
         "language": "html",
         "level": "beginner",
 	"read_by": []
-    }
+    },    {
+        "res_id": 5,
+        "title": "Introduction to CSS at MDN",
+        "url": "https://developer.mozilla.org/en-US/docs/Learn/CSS/Introduction_to_CSS",
+        "language": "css",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 6,
+        "title": "Introduction to CSS at CSSTutorial.net",
+        "url": "https://www.csstutorial.net/css-intro/introductioncss-part1.php",
+        "language": "css",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 7,
+        "title": "Sass basics",
+        "url": "http://sass-lang.com/guide",
+        "language": "css",
+        "level": "intermediate",
+	"read_by": []
+    },    {
+        "res_id": 8,
+        "title": "CSS Advanced",
+        "url": "http://marksheet.io/css-advanced.html",
+        "language": "css",
+        "level": "advanced",
+	"read_by": []
+    },    {
+        "res_id": 9,
+        "title": "Complete C# tutorial",
+        "url": "http://csharp.net-tutorials.com/basics/introduction/",
+        "language": "csharp",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 10,
+        "title": "Interactive C# tutorial at learncs.org",
+        "url": "http://www.learncs.org/",
+        "language": "csharp",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 11,
+        "title": "Intermediate C# tutorials",
+        "url": "http://rbwhitaker.wikidot.com/c-sharp-intermediate-tutorials",
+        "language": "csharp",
+        "level": "intermediate",
+	"read_by": []
+    },    {
+        "res_id": 12,
+        "title": "Advanced C#",
+        "url": "http://www.ssw.uni-linz.ac.at/Teaching/Lectures/CSharp/Tutorial/Part2.pdf",
+        "language": "csharp",
+        "level": "advanced",
+	"read_by": []
+    },    {
+        "res_id": 13,
+        "title": "Learn Perl",
+        "url": "http://www.learn-perl.org/",
+        "language": "perl",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 14,
+        "title": "Learn C",
+        "url": "http://www.learn-c.org/",
+        "language": "c",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 15,
+        "title": "Guide to Advanced Programming in C",
+        "url": "http://pfacka.binaryparadise.com/articles/guide-to-advanced-programming-in-C.html",
+        "language": "c",
+        "level": "advanced",
+	"read_by": []
+    },    {
+        "res_id": 16,
+        "title": "Advanced C",
+        "url": "https://www.e-reading.club/bookreader.php/138793/Advanced_C.pdf",
+        "language": "c",
+        "level": "advanced",
+	"read_by": []
+    },    {
+        "res_id": 17,
+        "title": "Agile Development Using Ruby on Rails - Advanced",
+        "url": "https://www.edx.org/course/agile-development-using-ruby-rails-uc-berkeleyx-cs169-2x-0",
+        "language": "ruby",
+        "level": "advanced",
+	"read_by": []
+    },    {
+        "res_id": 18,
+        "title": "Intro to React",
+        "url": "https://reactjs.org/tutorial/tutorial.html",
+        "language": "javascript",
+        "level": "beginner",
+	"read_by": []
+    },    {
+        "res_id": 19,
+        "title": "Advanced React and Redux",
+        "url": "https://www.udemy.com/react-redux-tutorial/",
+        "language": "javascript",
+        "level": "advanced",
+	"read_by": []
+    },
+
 ]
 
 try:
